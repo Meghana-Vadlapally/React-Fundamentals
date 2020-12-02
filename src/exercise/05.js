2a774f9d78fea851a81822ad4deafe257c356ffd
Meghana-Vadlapally <mvadlapa@buffalo.edu>
2020-12-02 18:27:35 -0500

    React Fundamentals

---
+++ src/exercise/05.js
@@ -0,0 +1,46 @@
+// Styling
+// http://localhost:3000/isolated/exercise/05.js
+
+import * as React from 'react'
+import '../box-styles.css'
+
+// 💰 Use the className for the size and style (backgroundColor) for the color
+// 💰 each of the elements should also have the "box" className applied
+
+// 🐨 add a className prop to each of these and apply the correct class names
+// 💰 Here are the available class names: box, box--large, box--medium, box--small
+
+// 🐨 add a style prop to each of them as well so their background color
+// matches what the text says it should be as well as `fontStyle: 'italic'`
+
+// const smallBox = <div className="box box--small">small lightblue box</div>
+// const mediumBox = <div className="box box--medium">medium pink box</div>
+// const largeBox = <div className="box box--large">large orange box</div>
+
+function Box({style, className = '', ...otherProps}) {
+  return (
+    <div
+      className={`box ${className}`}
+      style={{fontStyle: 'italic', ...style}}
+      {...otherProps}
+    />
+  )
+}
+
+function App() {
+  return (
+    <div>
+      <Box className="box--small" style={{backgroundColor: 'lightblue'}}>
+        small lightblue box
+      </Box>
+      <Box className="box--medium" style={{backgroundColor: 'pink'}}>
+        medium pink box
+      </Box>
+      <Box className="box--large" style={{backgroundColor: 'orange'}}>
+        large orange box
+      </Box>
+      <Box>sizeless box</Box>
+    </div>
+  )
+}
+export default App
---
+++ src/exercise/06.js
@@ -0,0 +1,40 @@
+// Basic Forms
+// http://localhost:3000/isolated/exercise/06.js
+
+import * as React from 'react'
+
+function UsernameForm({onSubmitUsername}) {
+  const [userName, setUserName] = React.useState('')
+
+  function handleSubmit(event) {
+    event.preventDefault()
+    onSubmitUsername(userName)
+  }
+
+  function handleChange(event) {
+    const {value} = event.target
+    setUserName(value.toLowerCase())
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="username">Username:</label>
+        <input
+          onChange={handleChange}
+          id="username"
+          type="text"
+          value={userName}
+        />
+      </div>
+      <button type="submit">Submit</button>
+    </form>
+  )
+}
+
+function App() {
+  const onSubmitUsername = username => alert(`You entered: ${username}`)
+  return <UsernameForm onSubmitUsername={onSubmitUsername} />
+}
+
+export default App
